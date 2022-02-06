5292620f642efd22074c397a2914d04116bdb1a1
jashan <jashandhiman.hulk@gmail.com>
2022-02-06 20:38:12 +0530

    started porfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import { toast } from "react-toastify";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  toast.info("hlo jas");
+  return <div>my portfolio</div>;
 }
 
 export default App;
