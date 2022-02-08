98149e19a81dcaa6b71cd84ef2630d1d828aa87d
jashan <jashandhiman.hulk@gmail.com>
2022-02-08 20:12:20 +0530

    add icons to show and hide menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoThreeBars } from "react-icons/go";
+import { ImCross } from "react-icons/im";
 
 const Navbar = () => {
+  const [openLink, setOpenLink] = useState(true);
+  //const handleOpenlinks = () => {};
+
   return (
     <div className="navbar">
-      <div className="heading">
-        <span>JD-Portfolio</span>
-      </div>
-      <div className="links">
-        <div className="openLinks">
-          <GoThreeBars />
-        </div>
+      {openLink ? (
+        <>
+          <div className="heading">
+            <span>JD-Portfolio</span>
+          </div>
+          {/*<div className="links">*/}
+          <div className="openLinks" onClick={() => setOpenLink(false)}>
+            <GoThreeBars />
+          </div>
+        </>
+      ) : (
         <div className="l_Icons">
-          <a href="/">Home</a>
+          <div id="cross" onClick={() => setOpenLink(true)}>
+            <ImCross />
+          </div>
           <a href="/">Resume</a>
           <a href="/">AboutMe</a>
           <a href="/">Testimonial</a>
           <a href="/">ContactMe</a>
         </div>
-      </div>
+        //</div>
+      )}
     </div>
   );
 };
