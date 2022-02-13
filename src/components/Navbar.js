10c3e37406781c7098876560387fb1736f6362bc
jashan <jashandhiman.hulk@gmail.com>
2022-02-13 22:54:18 +0530

    add functionality to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,83 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
 //import { GoThreeBars } from "react-icons/go";
-//import { ImCross } from "react-icons/im";
+import { ImCross } from "react-icons/im";
 //import { AiFillInstagram, AiFillGithub } from "react-icons/ai";
 import { BiHomeAlt, BiSend } from "react-icons/bi";
-import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineUser, AiOutlineAppstore } from "react-icons/ai";
 import { BsBriefcase, BsReverseLayoutTextSidebarReverse } from "react-icons/bs";
 
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+  /*==================== REMOVE MENU MOBILE ====================*/
+  function linkAction() {
+    setShowNav(false);
+  }
   return (
-    <header class="header" id="header">
-      <nav class="nav container">
-        <a href="/" class="nav_logo">
-          Jashan
-        </a>
-        <div class="nav_menu" id="nav-Menu">
-          <ul class="nav_list grid">
-            <li class="nav_item">
-              <a href="#home" class="nav_link">
-                <BiHomeAlt />
+    <header className="header" id="header">
+      {showNav ? (
+        <div className="nav_menu" id="nav-Menu">
+          <ul className="nav_list grid">
+            <li className="nav_item">
+              <a href="#home" className="nav_link" onClick={() => linkAction()}>
+                <i className="nav_icon">
+                  <BiHomeAlt />
+                </i>
                 Home
               </a>
             </li>
-            <li class="nav_item">
-              <a href="#about" class="nav_link">
-                <AiOutlineUser />
+            <li className="nav_item">
+              <a
+                href="#about"
+                className="nav_link"
+                onClick={() => linkAction()}
+              >
+                <i className="nav_icon">
+                  <AiOutlineUser />
+                </i>
                 About
               </a>
             </li>
-            <li class="nav_item">
-              <a href="#skills" class="nav_link">
-                <BsReverseLayoutTextSidebarReverse />
-                Skills
+            <li className="nav_item">
+              <a
+                href="#resume"
+                className="nav_link"
+                onClick={() => linkAction()}
+              >
+                <i className="nav_icon">
+                  <BsReverseLayoutTextSidebarReverse />
+                </i>
+                Resume
               </a>
             </li>
-            <li class="nav_item">
-              <a href="#services" class="nav_link">
-                <BsBriefcase />
+            <li className="nav_item">
+              <a
+                href="#services"
+                className="nav_link"
+                onClick={() => linkAction()}
+              >
+                <i className="nav_icon">
+                  <BsBriefcase />
+                </i>
                 Services
               </a>
             </li>
-            {/*<li class="nav_item">
-              <a href="#portfolio" class="nav_link">
-                Portfolio
-              </a>
-            </li>*/}
-            <li class="nav_item">
-              <a href="#contact" class="nav_link">
-                <BiSend />
+            <li className="nav_item">
+              <a
+                href="#contact"
+                className="nav_link"
+                onClick={() => linkAction()}
+              >
+                <i className="nav_icon">
+                  <BiSend />
+                </i>
                 ContactMe
               </a>
             </li>
           </ul>
-          <i class="uil uil-times nav_close" id="nav-Close"></i>
+          <i
+            className="nav_close"
+            id="nav-Close"
+            onClick={() => setShowNav(false)}
+          >
+            <ImCross />
+          </i>
         </div>
-        <div class="nav_btns">
-          <div class="nav_toggle" id="nav-Toggle">
-            <i class="uil uil-apps"></i>
+      ) : (
+        <nav className="nav container">
+          <a href="/" className="nav_logo">
+            Jashan
+          </a>
+          <div className="nav_btns">
+            <div
+              className="nav_toggle"
+              id="nav-Toggle"
+              onClick={() => setShowNav(true)}
+            >
+              <AiOutlineAppstore />
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      )}
     </header>
   );
 };
 
 export default Navbar;
 
-//<div className="navbar">
+//<div classNameName="navbar">
 //  {openLink ? (
 //    <>
-//      <div className="heading">
+//      <div classNameName="heading">
 //        <span>JD-Portfolio</span>
 //      </div>
-//      <div className="openLinks pointer" onClick={() => setOpenLink(false)}>
+//      <div classNameName="openLinks pointer" onClick={() => setOpenLink(false)}>
 //        <GoThreeBars />
 //      </div>
 //    </>
 //  ) : (
-//    <div className="l_Icons">
-//      <div id="cross" className="pointer" onClick={() => setOpenLink(true)}>
+//    <div classNameName="l_Icons">
+//      <div id="cross" classNameName="pointer" onClick={() => setOpenLink(true)}>
 //        <ImCross />
 //      </div>
 //      <a href="/">Resume</a>
